b753552d15fea78025ec5f444ec82e06a4d5b341
Mohana Raghul R <31816752+Raghul1909@users.noreply.github.com>
2019-08-06 13:59:37 +0530

    Update finalphp.php

--- finalphp.php
+++ finalphp.php
@@ -7,21 +7,21 @@ $uname=mysqli_fetch_assoc($uname);
 $uname=$uname['username'];
 if($uname!=0)
 {
-$qry="insert into tab(id,vote) values('$uname','$op1')";
-mysqli_query($con,$qry);
-$chk="SELECT username from tab WHERE username=$uname";
+$chk="SELECT id from tab WHERE id=$uname";
 $c=mysqli_query($con,$chk);
 $c=mysqli_fetch_assoc($c);
 $c=$c['username'];
+$qry="insert into tab(id,vote) values('$uname','$op1')";
+$qry=mysqli_query($con,$qry);
 }
 if (($c!=$uname)){
-if($qry && strlen($uname)==7)
+if($qry)
 {
 	$chk="SELECT email from mytable WHERE username=$uname";
 	$chk=mysqli_query($con,$sql);
 	$chk=mysqli_fetch_assoc($chk);
 	$chk=$chk['email'];
-	mail($chk,"Online Voting System","Your Vote Is Registereds");
+	mail($chk,"Online Voting System","Your Vote Is Registered");
 	$del="Truncate table sec";
 	mysqli_query($con,$del);
 	$sql="UPDATE mytable SET status='YES' WHERE id=$uname";
